43174720d4c798192d1bfae57a5aa43d13912abe
theFool <saber.rl32@gmail.com>
2018-11-13 14:31:36 +0800

    make completion item smartcase

--- rplugin/python3/deoplete/sources/en_en.py
+++ rplugin/python3/deoplete/sources/en_en.py
@@ -1,8 +1,8 @@
 from .base import Base
 import json
 
-class Source(Base):
 
+class Source(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
 
@@ -11,13 +11,16 @@ class Source(Base):
         self.min_pattern_length = 1
         self.filetypes = ['tex']
         path = '/'.join(__file__.split('/')[:-1])
-        with open(path+'/en-en-dict.json', 'r') as f:
+        with open(path + '/en-en-dict.json', 'r') as f:
             self.dict = json.load(f)
         self.words = self.dict.keys()
 
     def gather_candidates(self, context):
-        prefix = context['complete_str'].lower()
+        c_str = context['complete_str']
+        prefix = c_str.lower()
+        length = len(c_str)
         words = filter(lambda w: w.startswith(prefix), self.words)
-        return  [
-                {'word': w, 'info': self.dict[w]['description']} for w in words
-                ]
+        return [{
+            'word': c_str + w[length:],
+            'info': self.dict[w]['description']
+        } for w in words]
